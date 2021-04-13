550162519ec0819077a55843aab31efe3f864ae8
ppolle <peter.m.polle@gmail.com>
2021-04-13 12:05:58 +0300

    this commit will update functionality to get inidividual coin data

--- cryptoscraper/spiders/initial_scrape.py
+++ cryptoscraper/spiders/initial_scrape.py
@@ -16,3 +16,32 @@ class InitialScrapeSpider(scrapy.Spider):
     	if next_page is not None:
     		yield response.follow(next_page, callback=self.parse)
 
+    def get_coin_data(self, response):
+
+    	links = response.css('div.coin-link-row.mb-md-0')
+    	for link in links:
+    		if link.css('span.coin-link-title.mr-2::text').get() == 'Website':
+    			websites = link.css('a.coin-link-tag::attr(href)').getall()
+
+    		if link.css('span.coin-link-title.mr-2::text').get() == 'Tags':
+    			tags = link.css('a.coin-link-tag::attr(href)').getall() + link.css('span.coin-tag.mr-1::text').getall()
+
+    		if link.css('span.coin-link-title.mr-2::text').get() == 'Community':
+    			community = link.css('a.coin-link-tag::attr(href)').getall()
+
+    	yield {
+    	'Name': response.css('h1.mr-md-3.mx-2.mb-md-0.text-3xl.font-semibold::text').get(),
+    	'Website': websites,
+    	'Contract':,
+    	'Community': community,
+    	'Tags': tags,
+    	}
+
+    	for link in response.css('ul.coin-menu li.nav-item'):
+    		if link.css('a.font-weight-bold.nav-link::text').get() == 'Historical Data':
+    			historical_data_page = link.css('a.font-weight-bold.nav-link::attr(href)').get()
+    			yield response.follow(next_page, callback=self.get_historical_data)
+
+    def get_historical_data(self, response):
+    	pass
+
