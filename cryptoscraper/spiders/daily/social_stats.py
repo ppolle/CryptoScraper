080d7a0140a60ea8f0ba1d668df831b410aeb9bd
ppolle <peter.m.polle@gmail.com>
2021-04-15 13:08:11 +0300

    this commit will update social crawls

--- cryptoscraper/spiders/daily/social_stats.py
+++ cryptoscraper/spiders/daily/social_stats.py
@@ -17,5 +17,25 @@ class Daily/socialStatsSpider(scrapy.Spider):
         if next_page is not None:
         	yield response.follow(next_page, callback=self.parse)
 
-    def get_social_stat(self, response):
-    	pass
+    def get_social_stats(self, response):
+    	data= {}
+    	for social in response.css('div.center.p-3'):
+    		if social.css('div.uppercase::text').get() == 'Reddit Subscribers':
+    			data['redit_subscribers'] = social.css('div.mt-4.mb-2.text-2xl::text').get()
+
+    		if social.css('div.uppercase::text').get() == 'Average Accounts Active':
+    			data['active_redit_ac'] = social.css('div.mt-3.mb-2.text-xl::text').get()
+
+    		if social.css('div.uppercase::text').get() == 'Average New Hot Posts Per Hour':
+    			data['avg_posts_per_hr'] = social.css('div.mt-3.mb-2.text-xl::text').get()
+
+    		if social.css('div.uppercase::text').get() == 'Average New Comments Per Hour':
+    			data['avg_comments_per_hr'] = social.css('div.mt-3.mb-2.text-xl::text').get()
+
+    		if social.css('div.uppercase::text').get() == 'Twitter Followers':
+    			data['twitter_followers'] = social.css('div.mt-4.mb-2.text-2xl::text').get()
+
+    		if social.css('div.uppercase::text').get() == 'Telegram Users':
+    			data['telegram_users'] = social.css('div.mt-4.mb-2.text-2xl::text').get()
+
+    	yield data
