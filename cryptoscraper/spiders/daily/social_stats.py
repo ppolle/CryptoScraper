719c54e61b443c7cc6353441ea03e2b355594ede
ppolle <peter.m.polle@gmail.com>
2021-04-15 13:33:00 +0300

    this commit will update functinality to crawl coin socials

--- cryptoscraper/spiders/daily/social_stats.py
+++ cryptoscraper/spiders/daily/social_stats.py
@@ -1,13 +1,13 @@
 import scrapy
 
 
-class Daily/socialStatsSpider(scrapy.Spider):
+class DailySocialStatsSpider(scrapy.Spider):
     name = 'social_stats'
-    allowed_domains = ['www.coingecko.com/en']
+    allowed_domains = ['www.coingecko.com']
     start_urls = ['http://www.coingecko.com/en/']
 
     def parse(self, response):
-        coins = response.css('tr td.pl-1.pr-0 i::attr(data-coin-id)').getall()
+        coins = response.css('tr td.pl-1.pr-0 i::attr(data-coin-id)').getall()[:1]
 
         for coin in coins:
         	url = "https://www.coingecko.com/en/coins/{}/social_tab".format(coin)
@@ -20,22 +20,22 @@ class Daily/socialStatsSpider(scrapy.Spider):
     def get_social_stats(self, response):
     	data= {}
     	for social in response.css('div.center.p-3'):
-    		if social.css('div.uppercase::text').get() == 'Reddit Subscribers':
+    		if social.css('div.uppercase span::text').get() == 'Reddit Subscribers':
     			data['redit_subscribers'] = social.css('div.mt-4.mb-2.text-2xl::text').get()
 
-    		if social.css('div.uppercase::text').get() == 'Average Accounts Active':
+    		if social.css('div.uppercase span::text').get() == 'Average Accounts Active':
     			data['active_redit_ac'] = social.css('div.mt-3.mb-2.text-xl::text').get()
 
-    		if social.css('div.uppercase::text').get() == 'Average New Hot Posts Per Hour':
+    		if social.css('div.uppercase span::text').get() == 'Average New Hot Posts Per Hour':
     			data['avg_posts_per_hr'] = social.css('div.mt-3.mb-2.text-xl::text').get()
 
-    		if social.css('div.uppercase::text').get() == 'Average New Comments Per Hour':
+    		if social.css('div.uppercase span::text').get() == 'Average New Comments Per Hour':
     			data['avg_comments_per_hr'] = social.css('div.mt-3.mb-2.text-xl::text').get()
 
-    		if social.css('div.uppercase::text').get() == 'Twitter Followers':
+    		if social.css('div.uppercase span::text').get() == 'Twitter Followers':
     			data['twitter_followers'] = social.css('div.mt-4.mb-2.text-2xl::text').get()
 
-    		if social.css('div.uppercase::text').get() == 'Telegram Users':
+    		if social.css('div.uppercase span::text').get() == 'Telegram Users':
     			data['telegram_users'] = social.css('div.mt-4.mb-2.text-2xl::text').get()
 
     	yield data
