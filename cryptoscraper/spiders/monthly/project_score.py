cbff4a37f325a9a5f215e5cc8a15f7c0d1fe50e1
ppolle <peter.m.polle@gmail.com>
2021-04-15 18:01:55 +0300

    this commit will update project score crawl errors

--- cryptoscraper/spiders/monthly/project_score.py
+++ cryptoscraper/spiders/monthly/project_score.py
@@ -6,21 +6,30 @@ class ProjectScoreSpider(scrapy.Spider):
     start_urls = ['https://www.coingecko.com/en/']
 
     def parse(self, response):
-        coins = response.css('tr td.pl-1.pr-0 i::attr(data-coin-id)').getall()
+        coins = response.css('tr td.pl-1.pr-0 i::attr(data-coin-id)').getall()[:5]
 
         for coin in coins:
         	url = "https://www.coingecko.com/en/coins/{}/ratings_tab".format(coin)
         	yield response.follow(url, callback=self.get_project_score)
 
-        next_page = response.css('li.page-item.next a::attr(href)').get()
-        if next_page is not None:
-        	yield response.follow(next_page, callback=self.parse)
+        # next_page = response.css('li.page-item.next a::attr(href)').get()
+        # if next_page is not None:
+        # 	yield response.follow(next_page, callback=self.parse)
 
     def get_project_score(self, response):
-        yield {
-    	'Team Score':response.css('div.text-lg::text')[0].get(),
-    	'Eco System Score': response.css('div.text-lg::text')[1].get(),
-    	'Project Score': response.css('div.text-lg::text')[2].get(),
-    	'Outlook': response.css('div.text-lg::text')[3].get(),
-    	'Insight': response.css('div.text-3xs.mt-1::text').get(),
-    	}
+        try:
+            yield {
+        	'Team Score':response.css('div.text-lg::text')[0].get(),
+        	'Eco System Score': response.css('div.text-lg::text')[1].get(),
+        	'Project Score': response.css('div.text-lg::text')[2].get(),
+        	'Outlook': response.css('div.text-lg::text')[3].get(),
+        	'Insight': response.css('div.text-3xs.mt-1::text').get(),
+        	}
+        except IndexError:
+           yield {
+            'Team Score':None,
+            'Eco System Score': None,
+            'Project Score': None,
+            'Outlook': None,
+            'Insight': None,
+            }
