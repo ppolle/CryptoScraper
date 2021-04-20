5cbf83938618908d01aafe760b77370439978105
ppolle <peter.m.polle@gmail.com>
2021-04-20 19:34:16 +0300

    this commit will update normal utils file

--- cryptoscraper/utils.py
+++ cryptoscraper/utils.py
@@ -2,15 +2,15 @@ import re
 from datetime import datetime
 
 def get_slug(text):
-	return text[text.find("(")+1:text.find(")")]
+	return text[text.find("(")+1:text.find(")")].strip()
 
 def get_name(text):
-	return re.sub(r'\([^)]*\)|\n', '', text)
+	return re.sub(r'\([^)]*\)|\n', '', text).strip()
 
 def get_num(text):
 	if text is not None:
 		if any(char.isdigit() for char in text):
-			return  float(re.sub("[^0-9.]", "", text))
+			return  float(re.sub("[^0-9.]", "", text.strip()))
 		else:
 			return 0.0
 	else:
