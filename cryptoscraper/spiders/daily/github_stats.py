3f866408bc8fcf36cc68e767360ed1e24e7763eb
ppolle <peter.m.polle@gmail.com>
2021-04-20 19:10:34 +0300

    this commit will update normal crawling functionality

--- cryptoscraper/spiders/daily/github_stats.py
+++ cryptoscraper/spiders/daily/github_stats.py
@@ -7,21 +7,21 @@ class GithubStatsSpider(scrapy.Spider):
     start_urls = ['http://www.coingecko.com/en/']
 
     def parse(self, response):
-        coins = response.css('tr td.pl-1.pr-0 i::attr(data-coin-id)').getall()[:10]
+        coins = response.css('tr td.pl-1.pr-0 i::attr(data-coin-id)').getall()
 
         for coin in coins:
         	url = "https://www.coingecko.com/en/coins/{}/developer_tab".format(coin)
-        	yield response.follow(url, callback=self.get_github_stats, meta={'data_coin_id':get_num(coin)})
+        	yield response.follow(url, callback=self.get_github_stats, meta={'data_coin_id':int(coin)})
 
-        # next_page = response.css('li.page-item.next a::attr(href)').get()
-        # if next_page is not None:
-        # 	yield response.follow(next_page, callback=self.parse)
+        next_page = response.css('li.page-item.next a::attr(href)').get()
+        if next_page is not None:
+        	yield response.follow(next_page, callback=self.parse)
 
     def get_github_stats(self, response):
         data = GithubStatsItem()
         data['data_coin_id'] = response.meta['data_coin_id']
         for github in response.css('div.card-block'):
-            data['repo_name'] = get_num(github.css('span.text-xl a::text').get())
+            data['repo_name'] = github.css('span.text-xl a::text').get()
             data['url'] = github.css('span.text-xl a::attr(href)').get()
             data['stars'] = get_num(github.css('div.pt-2.pb-2.font-light::text')[0].get())
             data['watchers'] = get_num(github.css('div.pt-2.pb-2.font-light::text')[1].get())
