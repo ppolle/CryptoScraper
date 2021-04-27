1001993b137b938bf7b0cfe5a0662806bbe39455
ppolle <peter.m.polle@gmail.com>
2021-04-27 13:51:03 +0300

    this commit will update scripts to easily start scrapers from the crontabs

---
+++ scripts/daily_scraper.sh
@@ -0,0 +1,2 @@
+cd /home/cryptoscraper/coingecko && source venv/bin/activate
+cd crypto-scraper && python -c 'from tasks import daily_scraper; daily_scraper()'
--- tasks.py
+++ tasks.py
@@ -17,34 +17,25 @@ from scrapy.utils.project import get_project_settings
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 app.conf.beat_schedule = {
-    # 'daily-scraper': {
-    #     'task': 'tasks.daily_scraper',
-    #     'schedule': crontab(minute=0, hour=3),
-    # },
+    'daily-scraper': {
+        'task': 'tasks.daily_scraper',
+        'schedule': crontab(minute=0, hour=3),
+    },
     'monthly-scraper':{
 	    'task': 'tasks.monthly_scraper',
-	    # 'schedule': crontab(0,0,day_of_month='1'),
-        'schedule': crontab(minute='*/3')
+	    'schedule': crontab(0,0,day_of_month='1'),
     },
-    # 'initial-scrape':{
-    #     'task': 'tasks.initial_scraper',
-    #     'schedule': crontab(minute=40, hour=14,day_of_month='25'),
-    # },
+    'initial-scrape':{
+        'task': 'tasks.initial_scraper',
+        'schedule': crontab(minute=40, hour=14,day_of_month='25'),
+    },
+    'test-scrape':{
+    'task': 'tasks.testing_Scraper',
+    'schedule': crontab(minute='*/3'),
+    }
 }
 
 app.conf.timezone = 'UTC'
-
-class CrawlerProcess(Process):
-    def __init__(self, spider):
-        Process.__init__(self)
-        settings = get_project_settings()
-        self.crawler = Crawler(spider.__class__, settings)
-        self.crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-        self.spider = spider
-
-    def run(self):
-        self.crawler.crawl(self.spider)
-        reactor.run()
 
 @app.task
 def initial_scraper():
@@ -56,27 +47,19 @@ def initial_scraper():
 def daily_scraper():
     process = CrawlerProcess(get_project_settings())
     process.crawl(coin_stats.DailyCoinStatsSpider)
+    process.crawl(trending.TrendingSpider)
     process.crawl(daily_overall_metrics.DailyOverallMetricsSpider)
     process.crawl(github_stats.GithubStatsSpider)
-    process.crawl(trending.TrendingSpider)
     process.start()
 
 @app.task
 def monthly_scraper():
-    crawler = CrawlerProcess(trending.TrendingSpider)
-    crawler.start()
-    crawler.join()
-    # configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-    # runner = CrawlerRunner()
-    # d = runner.crawl(trending.TrendingSpider)
-    # d.addBoth(lambda _: reactor.stop())
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(ProjectScoreSpider)
+    process.start()
 
-    # for crawler in runner.crawlers:
-    #     crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-    # reactor.run()
-
-
-    # process = CrawlerProcess(get_project_settings())
-    # # process.crawl(ProjectScoreSpider)
-    # process.crawl(trending.TrendingSpider)
-    # process.start()
+@app.task
+def testing_scraper():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(trending.TrendingSpider)
+    process.start()
