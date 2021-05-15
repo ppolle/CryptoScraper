374d5252e6b074771042191082c440a76a65640b
ppolle <peter.m.polle@gmail.com>
2021-05-15 12:07:10 +0300

    this commit will update circulating supply and max supply as well as change circulating supply field from a string field to a float

---
+++ migrations/versions/7b47998b707e_update_circulating_supply_fields.py
@@ -0,0 +1,41 @@
+"""update circulating supply fields
+
+Revision ID: 7b47998b707e
+Revises: e5837d215cca
+Create Date: 2021-05-15 00:12:20.052286
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import orm
+from cryptoscraper.models import DailyCoinStats
+from cryptoscraper.utils import get_num
+
+
+# revision identifiers, used by Alembic.
+revision = '7b47998b707e'
+down_revision = 'e5837d215cca'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+
+    stats = session.query(DailyCoinStats).all()
+    for item in stats:
+      circulating_supply = item.circulating_supply
+      if circulating_supply is not None and '/' in circulating_supply:
+        split_circ = circulating_supply.split('/')
+        circ= get_num(split_circ[0])
+        max_supply = get_num(split_circ[1])
+        item.circulating_supply = circ
+        if item.max_supply is None or item.max_supply==0:
+          item.max_supply = max_supply
+
+    session.commit()
+
+
+def downgrade():
+    pass
