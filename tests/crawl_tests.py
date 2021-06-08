c1f7693383fefb5e984b58fa73ffad99ff275c66
ppolle <peter.m.polle@gmail.com>
2021-06-08 16:15:31 +0300

    this commit  will update initial crawl tests

--- tests/crawl_tests.py
+++ tests/crawl_tests.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker
-from cryptoscraper.models import Coin, DailyCoinStats,  db_connect
+from cryptoscraper.models import Coin, DailyCoinStats, DailyGithubMetrics, Trending, db_connect
 
 class Tests:
 	def __init__(self):
@@ -12,5 +12,26 @@ class Tests:
 		session = self.Session()
 		
 		coins = session.query(Coin).order_by(Coin.id).all()
-		todays_coin_stats = session.query(DailyCoinStats).filter_by(date=self.todays_date).all()
+		todays_coin_stats = session.query(DailyCoinStats).filter_by(date=self.todays_date).first()
+
+		session.close()
+		pass
+
+	def test_daily_github_metrics(self):
+		yesterdays_date=self.todays_date-1
+		yesterdays_metrics=session.query(DailyGithubMetrics).filter_by(date=yesterdays_date)
+		todays_metrics=session.query(DailyGithubMetrics).filter_by(date=self.todays_date)
+		if len(todays_metrics) < len(yesterdays_metrics):
+			print('Ni kunoma brather')
+			#find a way of finding the repositories that were not scrapped
+		pass
+
+	def test_trending(self):
+		session=self.Session()
+		trending = session.query(Trending).filter_by(date=self.todays_date)
+		if len(trending) >= 60:
+			print('Everythings good')
+
+		session.close()
+
 
